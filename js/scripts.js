3a8b2be52307cf4a6c445d9200062d45cb2fca61
SoloPimpa <sdarom.sdarom@gmail.com>
2022-06-23 19:03:43 +0300

    hamburger

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,24 @@ $(document).ready(function(){
 
     $('.owl-carousel').owlCarousel({
         loop:true,
+        autoplay:true,
+        autoplayTimeout: 1500,
         margin:0,
         nav:true,
         navText: [ '', ' ' ],
 
         responsive:{
             0:{
-                items:1
+                items:1,
             },
-
+            420:{
+                items:3
+            },
+            767: {
+                items: 3
+            },
             1000:{
-                items:1
+                items:5
             }
         }
     });
